8256866c9b7128228b1ad890b91e5e8ef4e29192
nicobustelo <nbustelo2002@gmail.com>
2024-07-05 22:05:18 -0300

    vercel configuration file - v4

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,6 +13,6 @@ export default defineConfig({
 		},
 	},
 	build: {
-		outDir: 'dist', // Ensure this matches your dist directory
+		outDir: 'frontend/dist', // Ensure this matches your dist directory
 	},
 });
