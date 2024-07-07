4938e48682a22d56016f8520f9a9188b01ae78d2
nicobustelo <nbustelo2002@gmail.com>
2024-07-07 20:50:39 -0300

    vercel configuration files - v12

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
+// server.js
 const express = require('express');
-const connectDB = require ('./config/db');
-const config = require('./utils/config.js');
+const connectDB = require('./config/db');
+const config = require('./utils/config');
 const middleware = require('./utils/middleware');
-const boilerplateRoutes = require('./routes/boilerplate.routes')
-const voteRoutes = require('./routes/vote.routes.js')
-const cors = require('cors')
+const boilerplateRoutes = require('./routes/boilerplate.routes');
+const voteRoutes = require('./routes/vote.routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -18,7 +19,7 @@ app.use(express.json());
 
 const PORT = config.PORT || 5000;
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}`);
+    console.log(`Server listening on port ${PORT}`);
 });
 
 app.use(middleware.requestLogger);
@@ -29,3 +30,5 @@ app.use('/api/votes', voteRoutes);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
+
+module.exports = app;
