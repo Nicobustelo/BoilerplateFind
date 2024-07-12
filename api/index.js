be1892286f236874a7dc748df23762ce82b553c0
nicobustelo <nbustelo2002@gmail.com>
2024-07-11 22:03:52 -0300

    vercel configuration files - v32

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
-const app = require('../backend/server');
-const port = process.env.PORT || 3000;
+import app from '../backend/server';
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
