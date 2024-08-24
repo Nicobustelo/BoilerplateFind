cd8da43153f8e7951ef7011f60875997ed3b38c7
nicobustelo <nbustelo2002@gmail.com>
2024-08-24 01:18:20 -0300

    Google analytics set up completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import ReactGA4 from 'react-ga4';
+import React, { useEffect } from 'react';
+
 import Home from './pages/Home';
 import Search from './pages/Search';
 import Leaderboards from './pages/Leaderboards';
 import Header from './components/Navbar';
 import Footer from './components/Footer';
 
+// Component to track route changes and send page views to GA4
+const RouteChangeTracker = () => {
+	const location = useLocation();
+  
+	useEffect(() => {
+	  ReactGA4.send({ hitType: 'pageview', page: location.pathname + location.search });
+	}, [location]);
+  
+	return null;
+  };
+
 export default function App() {
 	return (
 		<div className='bg-black text-white'>
 			<BrowserRouter>
+				<RouteChangeTracker />
 				<div className='h-full'>
 					<Header />
 					<Routes>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ReactGA4 from 'react-ga4'
+
+ReactGA4.initialize('G-0XDSHKSGX5')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
